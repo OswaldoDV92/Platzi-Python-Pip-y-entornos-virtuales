7dbc1311d2ddd3f2beb9b1a452271ff02e938487
OswaldoDV <oswaldodv92@gmail.com>
2023-02-24 08:30:40 -0600

    Pandas implementation for world graph.

--- app/graph_world_pop.py
+++ app/graph_world_pop.py
@@ -1,7 +1,14 @@
 import read_csv, charts
+import pandas as pd
 
 if __name__ == '__main__':
-    data = read_csv.read_csv('./data.csv')
-    percentage = list(map(lambda row: row['World Population Percentage'], data))
-    country_name = list(map(lambda row: row['Country/Territory'], data))
+    #data = read_csv.read_csv('./data.csv')
+    #percentage = list(map(lambda row: row['World Population Percentage'], data))
+    #country_name = list(map(lambda row: row['Country/Territory'], data))
+    
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'South America']
+    country_name = df['Country/Territory'].values
+    percentage = df['World Population Percentage'].values
+
     charts.generate_pie_chart('World', country_name, percentage)
