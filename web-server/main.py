1094eb7ce47bb9d6610b77a4dfc54adcd86df170
OswaldoDV <oswaldodv92@gmail.com>
2023-02-24 10:03:28 -0600

    Add a web server.

---
+++ web-server/main.py
@@ -0,0 +1,22 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1, 2, 3]
+
+@app.get('/contact', response_class = HTMLResponse)
+def get_list():
+    return """
+        <h1>Hola soy una página.</h1>
+        <p>Soy un párrafo.</p>
+    """
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
